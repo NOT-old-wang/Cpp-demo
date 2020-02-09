8bb7455cb8b349a22852c15e097c4ac01c91f2d1
wangjy <dawang5181314@163.com>
2020-02-09 16:34:40 +0800

    add thread order doing

--- thread/order_doing/demo.cc
+++ thread/order_doing/demo.cc
@@ -11,12 +11,14 @@ int main(int argc, const char** argv) {
   std::function<void(std::function<void()>)> third = std::bind(
       &FuncOrderManager::Third, &func_order_manager, std::placeholders::_1);
 
-  std::thread thread_a(first, DoingA);
+  std::thread thread_a1(first, DoingA);
+  std::thread thread_a2(first, DoingA);
   std::thread thread_b(second, DoingB);
   std::thread thread_c(third, DoingC);
 
   thread_c.join();
   thread_b.join();
-  thread_a.join();
+  thread_a1.join();
+  thread_a2.join();
   return 0;
 }
--- thread/order_doing/thread_test.h
+++ thread/order_doing/thread_test.h
@@ -3,23 +3,23 @@ Copyright [2019]
 */
 #ifndef THREAD_STL_TEST_THREAD_TEST_H_
 #define THREAD_STL_TEST_THREAD_TEST_H_
+#include <chrono>
 #include <functional>
 #include <mutex>
 #include <thread>
-#include <chrono>
 
-static void DoingA() { 
-  std::cout << "doing A" << std::endl; 
+static void DoingA() {
+  std::cout << "doing A" << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
-static void DoingB() { 
-  std::cout << "doing B" << std::endl; 
+static void DoingB() {
+  std::cout << "doing B" << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
 }
 
-static void DoingC() { 
-  std::cout << "doing C" << std::endl; 
+static void DoingC() {
+  std::cout << "doing C" << std::endl;
   std::this_thread::sleep_for(std::chrono::milliseconds(1500));
 }
 
@@ -28,34 +28,38 @@ static void DoingC() {
 */
 class FuncOrderManager {
  public:
-  FuncOrderManager() : count_(1){}
+  FuncOrderManager() : count_(1) {}
   void First(std::function<void()> fun) {
     std::unique_lock<std::mutex> lock(mutex_);
+    cv3_.wait(lock, [this]() { return count_ == 1; });
     std::cout << "first..." << std::endl;
     fun();
-    count_++;
+    count_ = 2;
     cv1_.notify_one();
   }
   void Second(std::function<void()> fun) {
     std::unique_lock<std::mutex> lock(mutex_);
-    cv1_.wait(lock, [this](){return count_==2;});
+    cv1_.wait(lock, [this]() { return count_ == 2; });
     std::cout << "second..." << std::endl;
-    count_++;
-    cv2_.notify_one();
     fun();
+    count_ = 3;
+    cv2_.notify_one();
   }
   void Third(std::function<void()> fun) {
     std::unique_lock<std::mutex> lock(mutex_);
-    cv2_.wait(lock, [this](){return count_==3;});
+    cv2_.wait(lock, [this]() { return count_ == 3; });
     std::cout << "third..." << std::endl;
     fun();
+    count_ = 1;
+    cv3_.notify_one();
   }
 
  private:
   std::mutex mutex_;
   std::condition_variable cv1_;
   std::condition_variable cv2_;
-  int count_;  
+  std::condition_variable cv3_;
+  int count_;
 };
 
 #endif  // THREAD_STL_TEST_THREAD_TEST_H_
