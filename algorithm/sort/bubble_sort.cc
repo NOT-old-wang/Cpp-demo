16d29da7265c76755012dc65b969d6d37f9e65a0
wangjy <dawang5181314@163.com>
2020-02-09 15:10:32 +0800

    optimize bubble sort

--- algorithm/sort/bubble_sort.cc
+++ algorithm/sort/bubble_sort.cc
@@ -2,20 +2,37 @@
 #include <vector>
 
 /*O(n 2)*/
-void BubbleSort(std::vector<int> *nums) {
-  for (int i = 0; i < nums->size(); i++) {
-    for (int j = i; j < nums->size(); j++) {
-      if ((*nums)[i] > (*nums)[j]) {
-        int temp = (*nums)[i];
-        (*nums)[i] = (*nums)[j];
-        (*nums)[j] = temp;
+// void BubbleSort(std::vector<int>* nums) {
+//   for (int i = 0; i < nums->size(); i++) {
+//     for (int j = i; j < nums->size(); j++) {
+//       if ((*nums)[i] > (*nums)[j]) {
+//         int temp = (*nums)[i];
+//         (*nums)[i] = (*nums)[j];
+//         (*nums)[j] = temp;
+//       }
+//     }
+//   }
+// }
+
+void BubbleSort(std::vector<int>* nums) {
+  if (nums->size() <= 1) return;
+  // 是否交换完成标志
+  bool flag = true;
+  int len = nums->size();
+  while (flag) {
+    flag = false;
+    for (int i = 1; i < len; i++) {
+      if ((*nums)[i - 1] > (*nums)[i]) {
+        std::swap((*nums)[i - 1], (*nums)[i]);
+        flag = true;
       }
     }
+    len--;
   }
 }
 
 int main(int argc, const char** argv) {
-  std::vector<int> test{3, 4, 2, 1, 9, 6, 5, 8 ,7};
+  std::vector<int> test{3, 4, 2, 1, 9, 6, 5, 8, 7};
   BubbleSort(&test);
   for (int num : test) {
     std::cout << num << ", ";
