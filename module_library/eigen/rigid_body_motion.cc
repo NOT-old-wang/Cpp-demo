7b038434a7a3fa4918eaea4adc634c068840a746
wangjy <dawang5181314@163.com>
2020-07-05 16:35:04 +0800

    fix bug

--- module_library/eigen/rigid_body_motion.cc
+++ module_library/eigen/rigid_body_motion.cc
@@ -9,44 +9,46 @@
  */
 
 int main(int argc, char** argv) {
-  std::cout << "hello-----" << std::endl;
-  // 输出精度为小数点后三位
-  std::cout.precision(3);
+  // 输出精度为小数点后1位
+  std::cout.precision(1);
   // 旋转矩阵 Identity就是设置为单位矩阵
   Eigen::Matrix3d rotation_matrix = Eigen::Matrix3d::Identity();
-  cout << "rotation_matrix = \n" << rotation_matrix << endl;
+  std::cout << "rotation_matrix = \n" << rotation_matrix << std::endl;
   // 旋转向量（轴向量）沿z轴45度, 逆时针为正
   Eigen::AngleAxisd rotation_vector(M_PI / 4, Eigen::Vector3d(0, 0, 1));
-  // rotation_vector.axis().transpose()
-  // rotation_vector.angle()
+  std::cout << "rotation_vector axis: " << rotation_vector.axis().transpose()
+            << std::endl;
+  std::cout << "rotation_vector angle: " << rotation_vector.angle()
+            << std::endl;
+
   /**** 旋转向量 <--> 四元数|欧拉角 <--> 旋转矩阵 互相转化相关 *****/
 
   // [旋转向量] <--> [旋转矩阵]
   std::cout << "rotation_matrix = \n" << rotation_vector.matrix() << std::endl;
   rotation_matrix = rotation_vector.toRotationMatrix();
   std::cout << "rotation_matrix = \n" << rotation_matrix << std::endl;
-  // rotation_vector = rotation_matrix.fromRotationMatrix();
 
   // [旋转向量] <--> [四元数]
   //  Eigen::Quaterniond q = Eigen::Quaterniond(1, 0, 0, 0);  //(w,x,y,z)
   Eigen::Quaterniond quaterniond = Eigen::Quaterniond(rotation_vector);
-  std::cout << "quaternion = \n" << quaterniond.coeffs() << std::endl;
+  std::cout << "quaternion = " << quaterniond.coeffs().transpose() << std::endl;
   // quaterniond.x() quaterniond.y() quaterniond.z() quaterniond.w()
   // rotation_vector = quaterniond;
 
   // [旋转矩阵] <--> [四元数]
   quaterniond = Eigen::Quaterniond(rotation_matrix);
-  std::cout << "quaternion = \n" << quaterniond.coeffs() << std::endl;
+  std::cout << "quaternion = " << quaterniond.coeffs().transpose() << std::endl;
   //   quaterniond.toRotationMatrix();
 
   // [旋转矩阵] <--> [欧拉角]
   // ZYX顺序，即yaw pitch roll顺序, (0,1,2)表示分别绕XYZ轴顺序，即pitch yaw
   // roll顺序，逆时针为正
   Eigen::Vector3d euler_angles = rotation_matrix.eulerAngles(2, 1, 0);
-  std::cout << "eulerAngles = \n" << euler_angles << std::endl;
-  std::cout << "yaw pitch roll = \n" << euler_angles.transpose();
-  std::cout << euler_angles[0] << "," << euler_angles[1] << ","
-            << euler_angles[2] << std::endl;
+  std::cout << "eulerAngles = \n" << euler_angles.transpose() << std::endl;
+  std::cout << "yaw pitch roll = \n" << euler_angles.transpose() << std::endl;
+  ;
+  //   std::cout << euler_angles[0] << "," << euler_angles[1] << ","
+  //             << euler_angles[2] << std::endl;
 
   // yaw
   Eigen::AngleAxisd angleAxis2 =
@@ -73,13 +75,13 @@ int main(int argc, char** argv) {
 
   // 使用四元数旋转一个向量
   v_rotated = quaterniond * v;
-  std::cout << v_rotated << std::endl;
+  // std::cout << v_rotated << std::endl;
 
   // 用旋转矩阵进行坐标变换
   v_rotated = rotation_matrix * v;
-  std::cout << "(1,0,0) after rotation = \n" << v_rotated << std::endl;
+  std::cout << "(1,0,0) after rotation = \n" << v_rotated.transpose() << std::endl;
 
-  // 用变换矩阵(旋转+平移)进行坐标变化
+  // 用欧式变换矩阵(旋转+平移)进行坐标变化
   // rotation 可以是旋转矩阵，可以是四元数，可以是旋转向量
   auto rotation = rotation_matrix;
   // Eigen:Isometry3d T(rotation);
@@ -96,7 +98,7 @@ int main(int argc, char** argv) {
   T.translation(); // 返回旋转矩阵
   */
   Eigen::Vector3d v_transformed = T * v;
-  std::cout << "v_transformed = \n" << v_transformed << std::endl;
+  std::cout << "v_transformed = \n" << v_transformed.transpose() << std::endl;
 
   return 0;
 }
